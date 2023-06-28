a69440a98ecfd1599e254c7d87eba9a826782168
Felipe Palma <felipepalmacastro@gmail.com>
2023-06-28 11:28:14 -0400

    Specimen created and proyected.

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/felipepalma1/8888_UN_backyardbreeders_XZ/models"
+)
 
 func main() {
+	var specimen = &models.Information{Name: "Killer S3372", Age: "2 Years", Weight: "15 Kilograms", Genre: "Female"}
 	fmt.Println("Backyard Breeders")
+	output := fmt.Sprintf("New specimen %s %s %s %s", specimen.Name, specimen.Age, specimen.Weight, specimen.Genre)
+	fmt.Println(output)
 }
---
+++ models/belgian_mallanois.go
@@ -0,0 +1,8 @@
+package models
+
+type Information struct {
+	Name   string
+	Age    string
+	Weight string
+	Genre  string
+}

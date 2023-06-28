15eacf4126ac3e9b72f74a272892bf6972e413f3
Felipe Palma <felipepalmacastro@gmail.com>
2023-06-28 11:52:56 -0400

    Specimen created and proyected.

---
+++ controllers/entanglementController.go
@@ -0,0 +1,7 @@
+package controllers
+
+import "fmt"
+
+func GetEntanglement() {
+	fmt.Println("Im Scared of REDACTED implementatios of GKE")
+}
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/felipepalma1/8888_UN_backyardbreeders_XZ/controllers"
+)
 
 func main() {
 	fmt.Println("Backyard Breeders")
+	controllers.GetSpecimenProfilePicture()
+	controllers.GetEntanglement()
+	controllers.GetSession()
+
 }
